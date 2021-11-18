b9045a7548f135ba2c28806aaf029e5ea7403a4c
shou-watanabe <watanabeshou1520@gmail.com>
2021-11-18 10:57:34 +0900

    fix function name

--- domain/repository/block.go
+++ domain/repository/block.go
@@ -5,8 +5,8 @@ import "go-blockchain/domain/entity"
 type BlockRepository interface {
 	PreviousHash(b *entity.Block) [32]byte
 	Nonce(b *entity.Block) int
-	Transaction(b *entity.Block) []*entity.Transaction
-	Print(b *entity.Block)
+	Transactions(b *entity.Block) []*entity.Transaction
+	Print(b *entity.Block, tr TransactionRepository)
 	Hash(b *entity.Block) [32]byte
 	MarshalJSON(b *entity.Block) ([]byte, error)
 	UnmarshalJSON(b *entity.Block, data []byte) error
