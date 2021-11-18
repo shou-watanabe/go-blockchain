471c3104f97c6833bdc4fd510627ae2c1b84c08d
shou-watanabe <watanabeshou1520@gmail.com>
2021-11-19 03:33:15 +0900

    complete wallet refactoring

--- wallet/main.go
+++ wallet/main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"flag"
 	"log"
+
+	"go-blockchain/wallet/infra/repository"
 )
 
 func init() {
@@ -10,10 +12,14 @@ func init() {
 }
 
 func main() {
+	wsr := repository.NewWalletServerRepository()
+	wr := repository.NewWalletRepository()
+	tr := repository.NewTransactionRepository()
+
 	port := flag.Uint("port", 8080, "TCP Port Number for Wallet Server")
 	gateway := flag.String("gateway", "http://127.0.0.1:5000", "Blockchain Gateway")
 	flag.Parse()
 
-	app := NewWalletServer(uint16(*port), *gateway)
-	app.Run()
+	ws := repository.NewWalletServer(uint16(*port), *gateway)
+	wsr.Run(ws, wr, tr)
 }
